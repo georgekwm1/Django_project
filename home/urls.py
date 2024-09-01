f8074a552320f580ca4fd38a8c48d24c20fbc205
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-01 18:53:17 +0100

    "Updated urls.py to change welcome route, and modified notes_list.html template"

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('welcome/', views.welcome, name='welcome'),
+    path('', views.welcome, name='welcome'),
     # path('welcome/', views.HomeView.as_view(), name='welcome'),
     path('authorized/', views.AuthorizedView.as_view()),
 ]
